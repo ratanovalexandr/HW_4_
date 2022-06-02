0fd5d142a3fd40be96f4b778a01577526e6d42ab
ratanovalexandr <ratanovalexandr@gmail.com>
2022-06-02 17:21:51 +0300

    added index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+// Исправленный вариант
 const firstNumber = Number(prompt('Enter first number'));
 
 const operation = prompt('Enter operator');
@@ -6,24 +6,45 @@ const operation = prompt('Enter operator');
 const secondNumber = Number(prompt('Enter second number'));
 
 
-
-if (operation ==='-') {
-  x = (firstNumber - secondNumber)
-  alert ('Ваш результат:  ' + x );
+if (isNaN(firstNumber) || isNaN(secondNumber)){ // проверка на число
+  alert ('Некорректный ввод данных');
 
 } else if (operation ==='+'){
-  x = (firstNumber + secondNumber)
-  alert ('Ваш результат:  ' + x );
- 
-}else if (operation ==='*'){
-  x = (firstNumber * secondNumber)
-  alert ('Ваш результат:  ' + x );
- 
+  alert ('Ваш результат:  '+ (firstNumber+secondNumber));
 
-}else if (operation ==='/'){
-  x = (firstNumber / secondNumber)
-  alert ('Ваш результат:  ' + x );
+} else if (operation ==='-'){
+  alert ('Ваш результат:  '+ (firstNumber-secondNumber));
 
-}else {
-  alert ('Taкой операции не существует');
+} else if (operation ==='/'){
+    alert ('Ваш результат:  '+ (firstNumber/secondNumber));
+
+} else if (operation ==='*'){
+      alert ('Ваш результат:  '+ (firstNumber*secondNumber));
+} else {
+  alert ('Такой операции не существует');
 }
+
+
+
+// Старый вариант
+
+// if (operation ==='-') {
+//   x = (firstNumber - secondNumber)
+//   alert ('Ваш результат:  ' + x );
+
+// } else if (operation ==='+'){
+//   x = (firstNumber + secondNumber)
+//   alert ('Ваш результат:  ' + x );
+ 
+// }else if (operation ==='*'){
+//   x = (firstNumber * secondNumber)
+//   alert ('Ваш результат:  ' + x );
+ 
+
+// }else if (operation ==='/'){
+//   x = (firstNumber / secondNumber)
+//   alert ('Ваш результат:  ' + x );
+
+// }else {
+//   alert ('Taкой операции не существует');
+// }
